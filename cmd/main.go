35019c22b9393c431ca0dcaaf08583b1c053a42c
andrey-shlygin-indriver <andrey.shlygin@indriver.com>
2023-06-19 11:18:35 +0300

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/hrumst/gox-lox/lib"
+)
+
+func main() {
+	lib.HelloLOX()
+}
---
+++ lib/run.go
@@ -0,0 +1,7 @@
+package lib
+
+import "fmt"
+
+func HelloLOX() {
+	fmt.Println("Hello, LOX")
+}
